331e4ed99b757b4ad677b68b5b6e9695b2c34d37
Elizabeth <liz@caspiantechnologies.org>
2017-12-14 16:53:29 +0700

    Added bluetooth screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {NgModule} from '@angular/core';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {BluetoothScreen} from './screens/bluetooth/bluetooth.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ToolbarComponent,
+        BluetoothScreen
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent,
+        ToolbarComponent,
+        BluetoothScreen]
 })
-export class AppModule { }
+
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ src/app/screens/bluetooth/bluetooth.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main',
+  templateUrl: './bluetooth.component.html',
+  styleUrls: ['./bluetooth.component.css']
+})
+export class BluetoothScreen {
+    myColor = 'warn';
+    valueBluetooth = '0';
+    bluetoothIcon = 'bluetooth_disabled';
+    spinnerClass = 'invisible bluetooth-spinner'
+  
+    changeBluetoothStatus(element){
+        console.log(this.myColor);
+        switch(this.valueBluetooth) {
+            case '0': { //disable
+                this.myColor = "primary";
+                this.valueBluetooth = "1";
+                this.bluetoothIcon = 'bluetooth';
+                this.spinnerClass = 'invisible bluetooth-spinner';
+                break;
+            }
+            case '1': { //enable
+                this.myColor = "accent";
+                this.valueBluetooth = "2";
+                this.bluetoothIcon = 'bluetooth_searching';
+                this.spinnerClass = 'bluetooth-spinner';
+                break;
+            }
+            case '2': { //search
+                this.myColor = "warn";
+                this.valueBluetooth = "0";
+                this.bluetoothIcon = 'bluetooth_disabled';
+                this.spinnerClass = 'invisible bluetooth-spinner';
+                break;
+            }
+        }
+    }
+}
