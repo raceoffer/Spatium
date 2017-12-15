2de3bf4ea1928bece28748f8ee060179b167ad86
Larisa <lora@caspiantechnologies.org>
2017-12-15 12:48:50 +0700

    Add Cordova dependency to Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var cordova: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  message = 'Loading...';
+  platform = cordova.platformId;
+
+  ngOnInit() {
+  	this.message = cordova.platformVersion;
+  	document.addEventListener("deviceready", () => {
+      console.log('Using Cordova plugins with Angular. Cordova version: ' + cordova.platformVersion)
+    }, false)
+  }
+
 }
