064309f0ba0b67932ab37fb052745927402948c7
Alexander <cat@caspiantechnologies.org>
2018-01-14 10:21:48 +0700

    Scaffold authentication

---
+++ src/app/screens/initiator-auth/initiator-auth.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {BitcoinKeyFragmentService} from '../../services/bitcoin-key-fragment.service';
+import {Router} from '@angular/router';
+import {WalletService} from '../../services/wallet.service';
+
+@Component({
+  selector: 'app-initiator-auth',
+  templateUrl: './initiator-auth.component.html',
+  styleUrls: ['./initiator-auth.component.css']
+})
+export class InitiatorAuthComponent implements OnInit {
+  entry = 'initiator-auth';
+  create = 'initiator-auth';
+
+  constructor(private router: Router,
+              private bitcoinKeyFragmentService: BitcoinKeyFragmentService,
+              private walletService: WalletService) { }
+
+  ngOnInit() {}
+
+  async onEntryClicked() {}
+
+  async onCreateClicked() {}
+}
