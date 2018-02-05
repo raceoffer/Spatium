5b49bd74fdee4274ab562de12f18a2915d9a3832
Larisa <lora@caspiantechnologies.org>
2018-02-05 16:06:25 +0700

    Add some currency icons.

--- src/app/screens/wallet/wallet.component.ts
+++ src/app/screens/wallet/wallet.component.ts
@@ -11,13 +11,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class WalletComponent{
 
   tiles = [
-    {title: 'Bitcoin', cols: 2, rows: 2},
-    {title: 'Ethereum', cols: 1, rows: 1},
-    {title: 'Ripple', cols: 1, rows: 1},
+    {title: 'Ethereum', cols: 2, rows: 2, logo: 'ethereum'},
+    {title: 'Bitcoin', cols: 1, rows: 1, logo: 'btc'},
+    {title: 'Litecoin', cols: 1, rows: 1, logo: 'litecoin'},
     {title: 'Bitcoin Cash', cols: 1, rows: 1},
     {title: 'Cardano', cols: 1, rows: 1},
     {title: 'NEO', cols: 1, rows: 1},
-    {title: 'Litecoin', cols: 1, rows: 1, logo: './assets/images/drawable/currency/litecoin.svg'},
+    {title: 'Ripple', cols: 1, rows: 1},
     {title: 'Stellar', cols: 2, rows: 2},
     {title: 'EOS', cols: 1, rows: 1},
     {title: 'NEM', cols: 1, rows: 1}
