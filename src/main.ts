8303c6519161a22b7a26392e64f2089cc1208b27
Alexander <cat@caspiantechnologies.org>
2018-02-07 13:07:49 +0700

    Remove non-cordova fallback

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-declare const cordova: any;
-
-// cordova.js has to be added after angular scripts
 const cordovaScript = document.createElement('script');
 cordovaScript.setAttribute('src', 'cordova.js');
 document.body.appendChild(cordovaScript);
 
 document.addEventListener('deviceready', () => {
+  if (environment.production) {
+    enableProdMode();
+  }
+
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
 }, false);
-
-if (typeof cordova === 'undefined') {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.log(err));
-}
-
-if (environment.production) {
-  enableProdMode();
-}
