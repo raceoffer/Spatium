320848c17f4719ee4bc6a82cd9c89ca21819292f
Alexander <cat@caspiantechnologies.org>
2018-02-13 18:41:02 +0700

    Decompose wallet service onto smaller entities.

---
+++ src/app/services/wallet/currencywallet.ts
@@ -0,0 +1,227 @@
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { BluetoothService } from '../bluetooth.service';
+import { LoggerService } from '../logger.service';
+import { SynchronizationStatus, SyncSession } from './syncsession';
+import { SignSession } from './signingsession';
+import { Subject } from 'rxjs/Subject';
+import { Coin, KeyChainService } from '../keychain.service';
+
+declare const CompoundKey: any;
+
+export enum Status {
+  None = 0,
+  Cancelled,
+  Failed,
+  Synchronizing,
+  Ready
+}
+
+export class CurrencyWallet {
+  protected compoundKey: any = null;
+
+  protected syncSession: SyncSession = null;
+  protected signSession: SignSession = null;
+
+  public status: BehaviorSubject<Status> = new BehaviorSubject<Status>(Status.None);
+  public synchronizing: Observable<boolean> = this.status.map(status => status === Status.Synchronizing);
+  public ready: Observable<boolean> = this.status.map(status => status === Status.Ready);
+
+  public statusChanged: Observable<Status> = this.status.skip(1).distinctUntilChanged();
+
+  public synchronizingEvent: Observable<any> = this.statusChanged.filter(status => status === Status.Synchronizing).mapTo(null);
+  public cancelledEvent: Observable<any> = this.statusChanged.filter(status => status === Status.Cancelled).mapTo(null);
+  public failedEvent: Observable<any> = this.statusChanged.filter(status => status === Status.Failed).mapTo(null);
+  public readyEvent: Observable<any> = this.statusChanged.filter(status => status === Status.Ready).mapTo(null);
+
+  public verifyEvent: Subject<any> = new Subject<any>();
+  public signedEvent: Subject<any> = new Subject<any>();
+  public acceptedEvent: Subject<any> = new Subject<any>();
+  public rejectedEvent: Subject<any> = new Subject<any>();
+
+  public syncProgress: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  public address: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  public balance: BehaviorSubject<any> = new BehaviorSubject<any>({ confirmed: 0, unconfirmed: 0 });
+
+  constructor(
+    protected network: string,
+    private keychain: KeyChainService,
+    private currency: Coin,
+    private account: number,
+    private messageSubject: any,
+    private bt: BluetoothService
+  ) {
+    this.bt.disconnectedEvent.subscribe(() => this.status.next(Status.None));
+
+    this.synchronizingEvent.subscribe(() => this.syncProgress.next(0));
+  }
+
+  public sync() {
+    if (this.status.getValue() === Status.Synchronizing) {
+      LoggerService.log('Sync in progress', {});
+      return;
+    }
+
+    this.compoundKey = new CompoundKey({
+      localPrivateKeyring: CompoundKey.keyringFromSecret(this.keychain.getCoinSecret(this.currency, this.account))
+    });
+
+    let prover = null;
+    try {
+      prover = this.compoundKey.startInitialCommitment();
+    } catch (e) {
+      LoggerService.nonFatalCrash('Failed to start initial commitment', e);
+      return;
+    }
+
+    this.status.next(Status.Synchronizing);
+    this.syncSession = new SyncSession(prover, this.messageSubject, this.bt);
+    this.syncSession.status.subscribe(state => {
+      this.syncProgress.next(
+        Math.max(Math.min(Math.round(state * 100 / (SynchronizationStatus.Finished - SynchronizationStatus.None + 1)), 100), 0)
+      );
+    });
+    this.syncSession.canceled.subscribe(() => {
+      console.log('received cancel event');
+      // pop the queue
+      this.messageSubject.next({});
+      this.status.next(Status.Cancelled);
+      this.syncSession = null;
+    });
+    this.syncSession.failed.subscribe(() => {
+      console.log('received failed event');
+      // pop the queue
+      this.messageSubject.next({});
+      this.status.next(Status.Failed);
+      this.syncSession = null;
+    });
+    this.syncSession.finished.subscribe(async (data) => {
+      console.log('received finished event');
+      // pop the queue
+      this.messageSubject.next({});
+      this.syncSession = null;
+      await this.finishSync(data);
+    });
+    // We'll handle it via events instead
+    this.syncSession.sync().catch(() => {});
+  }
+
+  public async reset() {
+    this.status.next(Status.None);
+
+    this.compoundKey = null;
+
+    if (this.syncSession) {
+      await this.syncSession.cancel();
+      this.syncSession = null;
+    }
+
+    if (this.signSession) {
+      await this.signSession.cancel();
+      this.signSession = null;
+    }
+
+    this.address.next('');
+    this.balance.next({ confirmed: 0, unconfirmed: 0 });
+  }
+
+  public async cancelSync() {
+    if (this.syncSession) {
+      await this.syncSession.cancel();
+    }
+  }
+
+  public async rejectTransaction() {
+    if (this.signSession) {
+      await this.signSession.cancel();
+    }
+  }
+
+  public async acceptTransaction() {
+    if (this.signSession) {
+      await this.signSession.submitChiphertexts();
+    }
+  }
+
+  public async finishSync(data) {
+    try {
+      this.compoundKey.finishInitialSync(data);
+    } catch (e) {
+      LoggerService.nonFatalCrash('Failed synchronization finish', e);
+    }
+  }
+
+  public async requestTransactionVerify(transaction) {
+    await this.bt.send(JSON.stringify({
+      type: 'verifyTransaction',
+      content:  transaction.toJSON()
+    }));
+
+    this.signSession = new SignSession(
+      transaction,
+      this.compoundKey,
+      this.messageSubject,
+      this.bt
+    );
+
+    this.signSession.ready.subscribe(async () => {
+      await this.signSession.awaitConfirmation();
+    });
+    this.signSession.canceled.subscribe(async () => {
+      console.log('canceled');
+      this.messageSubject.next({});
+      this.signSession = null;
+      this.rejectedEvent.next();
+    });
+    this.signSession.failed.subscribe(async () => {
+      console.log('failed');
+      this.messageSubject.next({});
+      this.signSession = null;
+      this.rejectedEvent.next();
+    });
+    this.signSession.signed.subscribe(async () => {
+      console.log('signed');
+      this.messageSubject.next({});
+      this.acceptedEvent.next();
+      this.signedEvent.next();
+    });
+
+    this.signSession.sync().catch(() => {});
+  }
+
+  public async startTransactionVerify(transaction) {
+    this.signSession = new SignSession(
+      transaction,
+      this.compoundKey,
+      this.messageSubject,
+      this.bt
+    );
+
+    this.signSession.ready.subscribe(async () => {
+      this.verifyEvent.next(transaction);
+    });
+    this.signSession.canceled.subscribe(async () => {
+      this.messageSubject.next({});
+      this.signSession = null;
+      this.rejectedEvent.next();
+    });
+    this.signSession.failed.subscribe(async () => {
+      this.messageSubject.next({});
+      this.signSession = null;
+      this.rejectedEvent.next();
+    });
+
+    this.signSession.sync().catch(() => {});
+  }
+
+  public async verifySignature() {
+    let verify = false;
+
+    if (this.signSession) {
+      verify = this.signSession.transaction.verify();
+    }
+
+    return verify;
+  }
+}
---
+++ src/app/services/wallet/syncsession.ts
@@ -0,0 +1,248 @@
+import { EventEmitter } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { BluetoothService } from '../bluetooth.service';
+import { LoggerService } from '../logger.service';
+
+export enum SynchronizationStatus {
+  None = 0,
+  Started,
+  InitialCommitment,
+  InitialDecommitment,
+  VerifierCommitment,
+  ProverCommitment,
+  VerifierDecommitment,
+  ProverDecommitment,
+  Finished
+}
+
+export class SyncSession {
+  public status: BehaviorSubject<SynchronizationStatus> = new BehaviorSubject<SynchronizationStatus>(SynchronizationStatus.None);
+
+  private initialCommitmentObserver:    Observable<any>;
+  private initialDecommitmentObserver:  Observable<any>;
+  private verifierCommitmentObserver:   Observable<any>;
+  private proverCommitmentObserver:     Observable<any>;
+  private verifierDecommitmentObserver: Observable<any>;
+  private proverDecommitmentObserver:   Observable<any>;
+
+  public finished: EventEmitter<any> = new EventEmitter();
+  public canceled: EventEmitter<any> = new EventEmitter();
+  public failed:   EventEmitter<any> = new EventEmitter();
+
+  private cancelObserver: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  constructor(
+    private prover: any,
+    private messageSubject: ReplaySubject<any>,
+    private bt: BluetoothService
+  ) {
+    this.initialCommitmentObserver =
+      this.messageSubject
+        .filter(object => object.type === 'initialCommitment')
+        .map(object => object.content);
+
+    this.initialDecommitmentObserver =
+      this.messageSubject
+        .filter(object => object.type === 'initialDecommitment')
+        .map(object => object.content);
+
+    this.verifierCommitmentObserver =
+      this.messageSubject
+        .filter(object => object.type === 'verifierCommitment')
+        .map(object => object.content);
+
+    this.proverCommitmentObserver =
+      this.messageSubject
+        .filter(object => object.type === 'proverCommitment')
+        .map(object => object.content);
+
+    this.verifierDecommitmentObserver =
+      this.messageSubject
+        .filter(object => object.type === 'verifierDecommitment')
+        .map(object => object.content);
+
+    this.proverDecommitmentObserver =
+      this.messageSubject
+        .filter(object => object.type === 'proverDecommitment')
+        .map(object => object.content);
+
+    this.messageSubject
+      .filter(object => object.type === 'cancel')
+      .subscribe(() => this.cancelObserver.next(true));
+  }
+
+  public async cancel() {
+    this.cancelObserver.next(true);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'cancel',
+        content: {}
+      }))) {
+      LoggerService.nonFatalCrash('Failed to send cancel', null);
+    }
+  }
+
+  public async sync() {
+    const handleFailure = (message, exception) => {
+      LoggerService.nonFatalCrash(message, exception);
+      this.status.next(SynchronizationStatus.Finished);
+      this.failed.emit();
+      throw new Error(message);
+    };
+
+    const handleCancel = () => {
+      LoggerService.log('Cancelled', {});
+      this.status.next(SynchronizationStatus.Finished);
+      this.canceled.emit();
+      throw new Error('Cancelled');
+    };
+
+    this.status.next(SynchronizationStatus.Started);
+    let initialCommitment = null;
+    try {
+      initialCommitment = this.prover.getInitialCommitment();
+    } catch (e) {
+      return handleFailure('Failed to get initialCommitment', e);
+    }
+
+    LoggerService.log('Sending initialCommitment:', initialCommitment);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'initialCommitment',
+        content: initialCommitment
+      }))) {
+      return handleFailure('Failed to send initialCommitment', null);
+    }
+
+    const remoteInitialCommitment = await this.initialCommitmentObserver.take(1).takeUntil(this.cancelObserver).toPromise();
+    if (!remoteInitialCommitment) {
+      return handleCancel();
+    }
+
+    this.status.next(SynchronizationStatus.InitialCommitment);
+    LoggerService.log('Received remoteInitialCommitment', remoteInitialCommitment);
+    let initialDecommitment = null;
+    try {
+      initialDecommitment = this.prover.processInitialCommitment(remoteInitialCommitment);
+    } catch (e) {
+      return handleFailure('Failed to process remoteInitialCommitment', e);
+    }
+
+    LoggerService.log('Sending initialDecommitment', initialDecommitment);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'initialDecommitment',
+        content: initialDecommitment
+      }))) {
+      return handleFailure('Failed to send initialDecommitment', null);
+    }
+
+    const remoteInitialDecommitment = await this.initialDecommitmentObserver.take(1).takeUntil(this.cancelObserver).toPromise();
+    if (!remoteInitialDecommitment) {
+      return handleCancel();
+    }
+
+    this.status.next(SynchronizationStatus.InitialDecommitment);
+    LoggerService.log('Received remoteInitialDecommitment', remoteInitialDecommitment);
+    let verifier = null;
+    try {
+      verifier = this.prover.processInitialDecommitment(remoteInitialDecommitment);
+    } catch (e) {
+      return handleFailure('Failed to process remoteInitialDecommitment', e);
+    }
+
+    const verifierCommitment = verifier.getCommitment();
+
+    LoggerService.log('Sending verifierCommitment', verifierCommitment);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'verifierCommitment',
+        content: verifierCommitment
+      }))) {
+      return handleFailure('Failed to send verifierCommitment', null);
+    }
+
+    const remoteVerifierCommitment = await this.verifierCommitmentObserver.take(1).takeUntil(this.cancelObserver).toPromise();
+    if (!remoteVerifierCommitment) {
+      return handleCancel();
+    }
+
+    this.status.next(SynchronizationStatus.VerifierCommitment);
+    LoggerService.log('Received remoteVerifierCommitment', remoteVerifierCommitment);
+    let proverCommitment = null;
+    try {
+      proverCommitment = this.prover.processCommitment(remoteVerifierCommitment);
+    } catch (e) {
+      return handleFailure('Failed to process remoteVerifierCommitment', e);
+    }
+
+    LoggerService.log('Sending proverCommitment', proverCommitment);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'proverCommitment',
+        content: proverCommitment
+      }))) {
+      return handleFailure('Failed to send proverCommitment', null);
+    }
+
+    const remoteProverCommitment = await this.proverCommitmentObserver.take(1).takeUntil(this.cancelObserver).toPromise();
+    if (!remoteProverCommitment) {
+      return handleCancel();
+    }
+
+    this.status.next(SynchronizationStatus.ProverCommitment);
+    LoggerService.log('Received remoteProverCommitment', remoteProverCommitment);
+    let verifierDecommitment = null;
+    try {
+      verifierDecommitment = verifier.processCommitment(remoteProverCommitment);
+    } catch (e) {
+      return handleFailure('Failed to process remoteProverCommitment', e);
+    }
+
+    LoggerService.log('Sending verifierDecommitment', verifierDecommitment);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'verifierDecommitment',
+        content: verifierDecommitment
+      }))) {
+      return handleFailure('Failed to send verifierDecommitment', null);
+    }
+
+    const remoteVerifierDecommitment = await this.verifierDecommitmentObserver.take(1).takeUntil(this.cancelObserver).toPromise();
+    if (!remoteVerifierDecommitment) {
+      return handleCancel();
+    }
+
+    this.status.next(SynchronizationStatus.VerifierDecommitment);
+    LoggerService.log('Received remoteVerifierDecommitment', remoteVerifierDecommitment);
+    let proverDecommitment = null;
+    try {
+      proverDecommitment = this.prover.processDecommitment(remoteVerifierDecommitment);
+    } catch (e) {
+      return handleFailure('Failed to process remoteVerifierDecommitment', e);
+    }
+
+    LoggerService.log('Sending proverDecommitment', proverDecommitment);
+    if (!await this.bt.send(JSON.stringify({
+        type: 'proverDecommitment',
+        content: proverDecommitment
+      }))) {
+      return handleFailure('Failed to send proverDecommitment', null);
+    }
+
+    const remoteProverDecommitment = await this.proverDecommitmentObserver.take(1).takeUntil(this.cancelObserver).toPromise();
+    if (!remoteProverDecommitment) {
+      return handleCancel();
+    }
+
+    this.status.next(SynchronizationStatus.ProverDecommitment);
+    LoggerService.log('Received remoteProverDecommitment', remoteProverDecommitment);
+    let verifiedData = null;
+    try {
+      verifiedData = verifier.processDecommitment(remoteProverDecommitment);
+    } catch (e) {
+      return handleFailure('Failed to process remoteProverDecommitment', e);
+    }
+
+    this.status.next(SynchronizationStatus.Finished);
+    this.finished.emit(verifiedData);
+
+    return verifiedData;
+  }
+}
