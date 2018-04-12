d8505b2edc732e9e24b07c17e79c14f7338c693d
Alexander <cat@caspiantechnologies.org>
2018-04-12 17:05:39 +0700

    Add prodmode switch.

--- cordova/hooks/prepareAngularApp.js
+++ cordova/hooks/prepareAngularApp.js
@@ -16,18 +16,27 @@ module.exports = function(context) {
       );
   }
 
+  let prod = '';
+  if (context.opts.options && context.opts.options['production']) {
+    prod = '--prod ';
+  }
+
   console.log('Building Angular application into "./www" directory.');
 
+  const command = "ng build --aot " + prod + "--output-path cordova/www/ --base-href /android_asset/www/";
+
+  console.log('Command:', command);
+
   console.log(execSync(
-    "ng build --aot --output-path cordova/www/ --base-href /android_asset/www/",
+    command,
     {
       maxBuffer: 1024*1024,
       cwd: basePath + '/..'
     }).toString('utf8')
   );
 
-  var files = fs.readdirSync(baseWWW);
-  for (var i = 0; i < files.length; i++) {
+  const files = fs.readdirSync(baseWWW);
+  for (let i = 0; i < files.length; i++) {
     if (files[i].endsWith('.gz')) {
       fs.unlinkSync(baseWWW + '/' + files[i]);
     }
