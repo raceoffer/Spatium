8f463a3dd88ad8fefdd185a799f6a639c5f545b8
Roman <roman@caspiantechnologies.org>
2018-04-20 13:12:11 +0700

    Small fixes after merge with work/desktop

--- src/crypto-core-async/index.js
+++ src/crypto-core-async/index.js
@@ -1,11 +1,11 @@
-// const PromiseWorker = require('promise-worker');
+const PromiseWorker = require('promise-worker');
 
 CryptoCore = require('crypto-core');
 
-// CryptoCore.worker = new PromiseWorker(new Worker('webworker.bundle.js'));
-// CryptoCore.Utils = require('./lib/utils').set(CryptoCore.worker);
-// CryptoCore.CompoundKey = require('./lib/compoundkey').set(CryptoCore.worker);
-// CryptoCore.BitcoinTransaction = require('./lib/bitcointransaction').set(CryptoCore.worker);
-// CryptoCore.BitcoinCashTransaction = require('./lib/bitcoincashtransaction').set(CryptoCore.worker);
-// CryptoCore.LitecoinTransaction = require('./lib/litecointransaction').set(CryptoCore.worker);
-// CryptoCore.EthereumTransaction = require('./lib/ethereumtransaction').set(CryptoCore.worker);
+CryptoCore.worker = new PromiseWorker(new Worker('webworker.bundle.js'));
+CryptoCore.Utils = require('./lib/utils').set(CryptoCore.worker);
+CryptoCore.CompoundKey = require('./lib/compoundkey').set(CryptoCore.worker);
+CryptoCore.BitcoinTransaction = require('./lib/bitcointransaction').set(CryptoCore.worker);
+CryptoCore.BitcoinCashTransaction = require('./lib/bitcoincashtransaction').set(CryptoCore.worker);
+CryptoCore.LitecoinTransaction = require('./lib/litecointransaction').set(CryptoCore.worker);
+CryptoCore.EthereumTransaction = require('./lib/ethereumtransaction').set(CryptoCore.worker);
