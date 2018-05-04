d36be913fe72f6e53f7b5a0bb4e7c13614b9c3a4
Roman <roman@caspiantechnologies.org>
2018-05-04 12:48:45 +0700

    Code cleanup

--- src/app/screens/factors/qr-reader/qr-reader.component.ts
+++ src/app/screens/factors/qr-reader/qr-reader.component.ts
@@ -26,15 +26,9 @@ export class QrReaderComponent implements OnInit {
   @Output() startScanEvent: EventEmitter<any> = new EventEmitter<any>();
 
   qrcode: string = null;
-  canScanAgain = true;
-  classVideoContainer = '';
-  classVideo = '';
-
+  canScanAgain = false;
   camStarted = false;
-  selectedDevice = undefined;
-  availableDevices = [];
   text = 'Scan a QR-code';
-  spinnerClass = '';
   permissionCam = false;
 
   constructor(private readonly ngZone: NgZone,
@@ -42,11 +36,8 @@ export class QrReaderComponent implements OnInit {
   }
 
   async ngOnInit() {
-    this.canScanAgain = false;
-    this.classVideoContainer = '';
+    this.canScanAgain = true;
     this.qrcode = '';
-    this.classVideo = 'small-video';
-    this.spinnerClass = 'spinner-video-container';
 
     const permissions = cordova.plugins.permissions;
     permissions.hasPermission(permissions.CAMERA, (status) => this.ngZone.run(async () => {
@@ -59,12 +50,6 @@ export class QrReaderComponent implements OnInit {
       }
     }));
 
-  }
-
-
-  addVideoContainer() {
-    const el = document.querySelector('video');
-    el.setAttribute('poster', '#');
   }
 
   errorCam() {
@@ -88,69 +73,34 @@ export class QrReaderComponent implements OnInit {
   scanAgain() {
     this.startScanEvent.emit();
     this.canScanAgain = false;
-    // this.classVideoContainer = '';
     this.camStarted = true;
     this.clearEvent.emit();
     cordova.plugins.barcodeScanner.scan((result) => this.handleQrCodeResult(result),
       (error) => console.log('qr scan error: ' + JSON.stringify(error)),
-      {
-        preferFrontCamera : false, // iOS and Android
-        showFlipCameraButton : true, // iOS and Android
-        showTorchButton : true, // iOS and Android
-        torchOn: false, // Android, launch with the torch switched on (if available)
-        saveHistory: false, // Android, save scan history (default false)
-        prompt : 'Place a barcode inside the scan area', // Android
-        resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-        // formats : 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
-        // orientation : 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
-        disableAnimations : true, // iOS
-        disableSuccessBeep: false // iOS and Android
-    }
-  );
+        {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: false, // Android, save scan history (default false)
+          prompt : 'Place a barcode inside the scan area', // Android
+          resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          // formats : 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
+          // orientation : 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+    );
   }
-
-  displayCameras(cams: any[]) {
-    this.availableDevices = cams;
-
-    console.log(cams);
-
-    if (cams && cams.length > 0) {
-      this.spinnerClass = 'invisible';
-      this.selectedDevice = cams[1];
-      this.camStarted = true;
-    }
-  }
-
-  // async handleQrCodeResult(event) {
-  //   if (!this.isRepeatable) {
-  //     this.qrcode = event.toString();
-  //   } else {
-  //     const buffer = Buffer.from(event.toString(), 'hex');
-  //     if (this.isImport) {
-  //       try {
-  //         console.log(buffer);
-  //         const value = await CryptoCore.Utils.tryUnpackEncryptedSeed(buffer);
-  //         this.qrcode = value.toString('hex');
-  //         console.log(this.qrcode);
-  //       } catch (exc) {
-  //         console.log(exc);
-  //         this.qrcode = null;
-  //       }
-  //     } else {
-  //       this.qrcode = await CryptoCore.Utils.tryUnpackLogin(buffer);
-  //     }
-  //   }
-  //   await this.onNext();
-  // }
 
   async handleQrCodeResult(result) {
-    console.log('inside handleQrCodeResult');
-    if (result.text === null) {
-      console.log('result.text is null');
-      console.log('this.isRepeatable: ' + this.isRepeatable);
-      this.canScanAgain = true;
+    if (result.cancelled) {
+      this.ngZone.run(async () => {
+        this.canScanAgain = true;
+      });
       return;
     }
+
     if (!this.isRepeatable) {
       this.qrcode = result.text;
     } else {
@@ -177,7 +127,6 @@ export class QrReaderComponent implements OnInit {
 
     if (this.inputEvent) {
       this.canScanAgain = true;
-      this.classVideoContainer = 'invisible';
       this.inputEvent.emit(this.qrcode);
     }
 
