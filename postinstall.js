9e6639122ba1a298506721b014004a28c97dd931
Alexander <cat@caspiantechnologies.org>
2018-05-17 18:31:01 +0700

    Add a postinstall script.

---
+++ postinstall.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+console.log('running postinstall script to enable crypto');
+
+fs.readFile('./node_modules/@angular/cli/models/webpack-configs/browser.js', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  const result = data.replace(/crypto: 'empty',/g, 'crypto: true,');
+  fs.writeFile('./node_modules/@angular/cli/models/webpack-configs/browser.js', result, 'utf8', function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
--- src/app/services/worker.service.ts
+++ src/app/services/worker.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { createWorker } from 'crypto-core-async';
+import { createWorker } from 'crypto-core-async/lib/createworker';
 
 @Injectable()
 export class WorkerService {
