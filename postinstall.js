99aa79a788e68e588323acbd06d1628e1b4e8263
Alexander <cat@caspiantechnologies.org>
2018-05-18 12:47:41 +0700

    Restore a postinstall script.

--- postinstall.js
+++ postinstall.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
+const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 
-console.log('running postinstall script to enable crypto');
-
-fs.readFile('./node_modules/@angular/cli/models/webpack-configs/browser.js', 'utf8', function (err,data) {
+fs.readFile(f, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  const result = data.replace(/crypto: 'empty',/g, 'crypto: true,');
-  fs.writeFile('./node_modules/@angular/cli/models/webpack-configs/browser.js', result, 'utf8', function (err) {
-    if (err) {
-      return console.log(err);
-    }
+  var result = data.replace(/node: false/g, 'node: { crypto: true, stream: true, fs: \'empty\', net: \'empty\', tls: \'empty\' }');
+
+  fs.writeFile(f, result, 'utf8', function (err) {
+    if (err) return console.log(err);
   });
 });
