42875e5ed2adb412ff57e45e8bc5e43a114a3b24
Alexander <cat@caspiantechnologies.org>
2018-05-29 19:26:25 +0700

    Implement disabling workers for windows
    SPATIUM-259

--- src/app/services/worker.service.ts
+++ src/app/services/worker.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { DeviceService } from './device.service';
 
 import { createWorker } from 'crypto-core-async/lib/createworker';
 
+declare const device: any;
+
 @Injectable()
 export class WorkerService {
-  public worker = createWorker();
+  public worker = null;
+
+  constructor(private readonly deviceService: DeviceService) {
+    this.init();
+  }
+
+  async init() {
+    await this.deviceService.deviceReady();
+    
+    if (!device.isWindows) {
+      this.worker = createWorker();
+    }
+  }
 }
