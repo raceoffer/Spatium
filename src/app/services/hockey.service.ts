dbfb3e6027f793b624099cd7da01340acdda3ee2
Roman <roman@caspiantechnologies.org>
2018-06-26 11:34:44 +0700

    Add hockey appId for iOS
    SPATIUM-237

--- src/app/services/hockey.service.ts
+++ src/app/services/hockey.service.ts
@@ -12,7 +12,8 @@ export class HockeyService {
         return '6a66e9dc6499491187e1bb8c3bfeced9';
       case Platform.Windows:
         return 'e5c2e9e03a00418fb37a77c2fc291c51';
-      // TODO: return appId for iOS
+      case Platform.IOS:
+        return '34994df5452c4d5797f4009a8a6430d2';
       default:
         return '';
     }
