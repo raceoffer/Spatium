2005806db944459532aa098d8a7655072c2fb7e9
Roman <roman@caspiantechnologies.org>
2018-06-27 13:53:32 +0700

    Fix including wrong ionic-native-http-angular-wrapper source
    SPATIUM-249

--- ionic-native-http-angular-wrapper/dist/providers/http-wrapper.d.ts
+++ ionic-native-http-angular-wrapper/dist/providers/http-wrapper.d.ts
@@ -1,29 +1,19 @@
-import { HTTP } from '@ionic-native/http';
-import { HttpClient, HttpClient as angularHttp, HttpRequest } from "@angular/common/http";
+import { HTTP as nativeHttp } from '@ionic-native/http';
+import { Http as angularHttp, RequestOptionsArgs } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs";
 export declare class HttpWrapper {
     private native;
     private angular;
     protected nativeIsAvailable: boolean | null;
-    nativeHttp: HTTP;
-    angularHttp: HttpClient;
-    constructor(native: HTTP, angular: angularHttp);
+    nativeHttp: nativeHttp | any;
+    angularHttp: angularHttp | any;
+    forceAngular: boolean;
+    constructor(native: nativeHttp, angular: angularHttp);
     isNativeHttpAvailable(): boolean;
-    get(url: string, options?: HttpRequest<any>): Observable<any>;
-    post(url: string, body: any, options?: HttpRequest<any>): Observable<any>;
-    put(url: string, body: any, options?: HttpRequest<any>): Observable<any>;
-    delete(url: string, options?: HttpRequest<any>): Observable<any>;
-    private parseOptionsForAngularHttp(options);
-    private parseHeadersForNativeHttp(options);
-    private parseParamsForNativeHttp(options);
-    private parseBodyFromNativeHttpResponse(res, options);
-    /**
-     * @deprecated, use GET|PUT|POST|DELETE methods instead
-     * @param {string} url
-     * @param {HttpRequest} options
-     * @param {Object} data
-     * @returns {Observable}
-     */
-    request(url: string, options: HttpRequest<any>, data?: Object): Observable<any>;
+    get(url: string, options?: RequestOptionsArgs): Observable<any>;
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<any>;
+    put(url: string, body: any, options?: RequestOptionsArgs): Observable<any>;
+    delete(url: string, options?: RequestOptionsArgs): Observable<any>;
+    request(url: string, options: RequestOptionsArgs, data?: Object): Observable<any>;
 }
--- ionic-native-http-angular-wrapper/dist/providers/http-wrapper.js
+++ ionic-native-http-angular-wrapper/dist/providers/http-wrapper.js
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HTTP } from '@ionic-native/http';
-import { HttpClient, HttpHeaders, HttpRequest } from "@angular/common/http";
+import { HTTP as nativeHttp } from '@ionic-native/http';
+import { Headers, Http as angularHttp, RequestMethod } from "@angular/http";
 import { checkAvailability } from '@ionic-native/core';
 import { Observable } from "rxjs/Observable";
+import { from } from 'rxjs';
+import { map } from "rxjs/operators";
 import "rxjs";
 var HttpWrapper = /** @class */ (function () {
     function HttpWrapper(native, angular) {
         this.native = native;
         this.angular = angular;
         this.nativeIsAvailable = null;
+        this.nativeHttp = false;
+        this.angularHttp = false;
+        this.forceAngular = false;
         this.nativeHttp = native;
         this.angularHttp = angular;
     }
@@ -19,217 +24,118 @@ var HttpWrapper = /** @class */ (function () {
         return this.nativeIsAvailable;
     };
     HttpWrapper.prototype.get = function (url, options) {
-        var _this = this;
-        if (this.isNativeHttpAvailable()) {
-            console.log('native get');
-            return Observable.fromPromise(this.nativeHttp.get(url, this.parseParamsForNativeHttp(options), this.parseHeadersForNativeHttp(options))).map(function (res) {
-                return {
-                    json: function () {
-                        return JSON.parse(res.data);
-                    },
-                    text: function (ignoredEncodingHint) {
-                        return res.data.toString();
-                    },
-                    body: _this.parseBodyFromNativeHttpResponse(res, options),
-                    headers: new Headers(res.headers)
-                };
-            });
-        }
-        console.log('angular get');
-        return this.angularHttp.get(url, this.parseOptionsForAngularHttp(options));
+        options.method = RequestMethod.Get;
+        return this.request(url, options);
     };
     HttpWrapper.prototype.post = function (url, body, options) {
-        var _this = this;
-        if (this.isNativeHttpAvailable()) {
-            return Observable.fromPromise(this.nativeHttp.post(url, body, this.parseHeadersForNativeHttp(options))).map(function (res) {
-                return {
-                    json: function () {
-                        return JSON.parse(res.data);
-                    },
-                    text: function (ignoredEncodingHint) {
-                        return res.data.toString();
-                    },
-                    body: _this.parseBodyFromNativeHttpResponse(res, options),
-                    headers: new Headers(res.headers)
-                };
-            });
-        }
-        return this.angularHttp.post(url, body, this.parseOptionsForAngularHttp(options));
+        options.method = RequestMethod.Post;
+        return this.request(url, options, body);
     };
     HttpWrapper.prototype.put = function (url, body, options) {
-        var _this = this;
-        if (this.isNativeHttpAvailable()) {
-            return Observable.fromPromise(this.nativeHttp.put(url, body, this.parseHeadersForNativeHttp(options))).map(function (res) {
-                return {
-                    json: function () {
-                        return JSON.parse(res.data);
-                    },
-                    text: function (ignoredEncodingHint) {
-                        return res.data.toString();
-                    },
-                    body: _this.parseBodyFromNativeHttpResponse(res, options),
-                    headers: new Headers(res.headers)
-                };
-            });
-        }
-        return this.angularHttp.put(url, body, this.parseOptionsForAngularHttp(options));
+        options.method = RequestMethod.Put;
+        return this.request(url, options, body);
     };
     HttpWrapper.prototype.delete = function (url, options) {
-        var _this = this;
-        if (this.isNativeHttpAvailable()) {
-            return Observable.fromPromise(this.nativeHttp.delete(url, this.parseParamsForNativeHttp(options), this.parseHeadersForNativeHttp(options))).map(function (res) {
-                return {
-                    json: function () {
-                        return JSON.parse(res.data);
-                    },
-                    text: function (ignoredEncodingHint) {
-                        return res.data.toString();
-                    },
-                    body: _this.parseBodyFromNativeHttpResponse(res, options),
-                    headers: new Headers(res.headers)
-                };
-            });
-        }
-        return this.angularHttp.delete(url, this.parseOptionsForAngularHttp(options));
+        options.method = RequestMethod.Delete;
+        return this.request(url, options);
     };
-    HttpWrapper.prototype.parseOptionsForAngularHttp = function (options) {
-        var angularOptions = options;
-        if (options instanceof HttpRequest) {
-            angularOptions = {};
-            angularOptions.headers = options !== undefined && options.headers !== undefined ? options.headers : {};
-            angularOptions.params = options !== undefined && options.params !== undefined ? options.params : {};
-        }
-        if (angularOptions === undefined) {
-            angularOptions = {};
-            angularOptions.responseType = 'json';
-        }
-        if (angularOptions.responseType === undefined) {
-            angularOptions.responseType = 'json';
-        }
-        if (angularOptions.observe === undefined) {
-            angularOptions.observe = 'response';
-        }
-        return angularOptions;
-    };
-    HttpWrapper.prototype.parseHeadersForNativeHttp = function (options) {
-        var headers = options !== undefined && options.headers !== undefined ? options.headers : {};
-        if (headers instanceof Headers) {
-            var newHeaders_1 = {};
-            headers.forEach(function (value, name) {
-                newHeaders_1[name.toString()] = value.toString();
-            });
-            headers = newHeaders_1;
-        }
-        return headers;
-    };
-    HttpWrapper.prototype.parseParamsForNativeHttp = function (options) {
-        return options !== undefined && options.params !== undefined ? options.params : {};
-    };
-    HttpWrapper.prototype.parseBodyFromNativeHttpResponse = function (res, options) {
-        if (res.data) {
-            if (options === undefined || options.responseType === undefined || options.responseType === 'json') {
-                return JSON.parse(res.data);
-            }
-            return res.data;
-        }
-        return null;
-    };
-    /**
-     * @deprecated, use GET|PUT|POST|DELETE methods instead
-     * @param {string} url
-     * @param {HttpRequest} options
-     * @param {Object} data
-     * @returns {Observable}
-     */
     HttpWrapper.prototype.request = function (url, options, data) {
-        if (this.isNativeHttpAvailable()) {
+        if (this.isNativeHttpAvailable() && !this.forceAngular) {
             var headers = options.headers;
             if (headers instanceof Headers) {
-                var newHeaders_2 = {};
+                var newHeaders_1 = {};
                 headers.forEach(function (value, name) {
-                    newHeaders_2[name.toString()] = value.toString();
+                    newHeaders_1[name.toString()] = value.toString();
                 });
-                headers = newHeaders_2;
+                headers = newHeaders_1;
             }
             switch (options.method) {
-                case 'GET':
+                case RequestMethod.Get:
                     if (data == null) {
                         data = options.params;
                     }
-                    return Observable.fromPromise(this.nativeHttp.get(url, data, headers)).map(function (res) {
-                        return {
-                            json: function () {
-                                return JSON.parse(res.data);
-                            },
-                            text: function (ignoredEncodingHint) {
-                                return res.data.toString();
-                            },
-                            body: res.data,
-                            headers: new Headers(res.headers)
-                        };
-                    });
-                case 'POST':
-                    return Observable.fromPromise(this.nativeHttp.post(url, data, headers)).map(function (res) {
-                        return {
-                            json: function () {
-                                return JSON.parse(res.data);
-                            },
-                            text: function (ignoredEncodingHint) {
-                                return res.data.toString();
-                            },
-                            body: res.data,
-                            headers: new Headers(res.headers)
-                        };
-                    });
-                case 'PUT':
+                    return from(this.nativeHttp.get(url, data, headers)).pipe(
+                        map(function (res) {
+                            return {
+                                json: function () {
+                                    return JSON.parse(res.data);
+                                },
+                                text: function (ignoredEncodingHint) {
+                                    return res.data.toString();
+                                },
+                                data: res.data,
+                                headers: new Headers(res.headers)
+                            };
+                        })
+                    )
+                case RequestMethod.Post:
                     if (data == null) {
-                        data = options.params != null ? options.params : {};
+                        data = options.body != null ? options.body : {};
                     }
-                    return Observable.fromPromise(this.nativeHttp.put(url, data, headers)).map(function (res) {
-                        return {
-                            json: function () {
-                                return JSON.parse(res.data);
-                            },
-                            text: function (ignoredEncodingHint) {
-                                return res.data.toString();
-                            },
-                            body: res.data,
-                            headers: new Headers(res.headers)
-                        };
-                    });
-                case 'DELETE':
-                    return Observable.fromPromise(this.nativeHttp.delete(url, data, headers)).map(function (res) {
-                        return {
-                            json: function () {
-                                return JSON.parse(res.data);
-                            },
-                            text: function (ignoredEncodingHint) {
-                                return res.data.toString();
-                            },
-                            body: res.data,
-                            headers: new Headers(res.headers)
-                        };
-                    });
+                    return from(this.nativeHttp.post(url, data, headers)).pipe(
+                        map(function (res) {
+                            return {
+                                json: function () {
+                                    return JSON.parse(res.data);
+                                },
+                                text: function (ignoredEncodingHint) {
+                                    return res.data.toString();
+                                },
+                                data: res.data,
+                                headers: new Headers(res.headers)
+                            };
+                        })
+                    )
+                case RequestMethod.Put:
+                    if (data == null) {
+                        data = options.body != null ? options.body : {};
+                    }
+                    return from(this.nativeHttp.put(url, data, headers)).pipe(
+                        map(function (res) {
+                            return {
+                                json: function () {
+                                    return JSON.parse(res.data);
+                                },
+                                text: function (ignoredEncodingHint) {
+                                    return res.data.toString();
+                                },
+                                data: res.data,
+                                headers: new Headers(res.headers)
+                            };
+                        })
+                    )
+                case RequestMethod.Delete:
+                    if (data == null) {
+                        data = options.body != null ? options.body : {};
+                    }
+                    console.log('deleting with data:', data);
+                    return from(this.nativeHttp.delete(url, data, headers)).pipe(
+                        map(function (res) {
+                            return {
+                                json: function () {
+                                    return JSON.parse(res.data);
+                                },
+                                text: function (ignoredEncodingHint) {
+                                    return res.data.toString();
+                                },
+                                data: res.data,
+                                headers: new Headers(res.headers)
+                            };
+                        })
+                    )
                 default:
                     throw 'Request Method not found';
             }
         }
         else {
-            //Make an @angular/common/http request
-            var modifiedOptions = options.clone({
-                'url': url
-            });
+            //Make an @angular/http request
             if (options.headers === undefined) {
-                modifiedOptions = options.clone({
-                    "headers": new HttpHeaders()
-                });
+                options.headers = new Headers();
             }
             if (data) {
-                modifiedOptions = modifiedOptions.clone({
-                    "body": JSON.stringify(data)
-                });
+                options.body = JSON.stringify(data);
             }
-            return this.angularHttp.request(modifiedOptions);
+            return this.angularHttp.request(url, options);
         }
     };
     HttpWrapper.decorators = [
@@ -237,8 +143,8 @@ var HttpWrapper = /** @class */ (function () {
     ];
     /** @nocollapse */
     HttpWrapper.ctorParameters = function () { return [
-        { type: HTTP, },
-        { type: HttpClient, },
+        { type: nativeHttp, },
+        { type: angularHttp, },
     ]; };
     return HttpWrapper;
 }());
