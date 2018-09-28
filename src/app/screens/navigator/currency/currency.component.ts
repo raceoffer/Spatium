7b233d6ef40623520998728e99a839d31bc662db
Roman <roman@caspiantechnologies.org>
2018-09-28 15:43:18 +0700

    Fix displaying amount of transaction history items
    SPATIUM-639

--- src/app/screens/navigator/currency/currency.component.ts
+++ src/app/screens/navigator/currency/currency.component.ts
@@ -78,18 +78,6 @@ export class CurrencyComponent implements OnInit, OnDestroy {
   public isLoadingTransactions = false;
   private step = 10;
 
-  // public transactionArray = [];
-  // public transactions: BehaviorSubject<Array<HistoryEntry>> = null;
-  // public isLoadingTransactions = false;
-  // public isUpdatingTransactions = false;
-  // public timeEnd = null;
-
-  // private transactionsCount = 0;
-  // private isHistoryLoaded = false;
-  // private step = 10;
-  // private unconfirmedList: Array<HistoryEntry> = [];
-  // private isFirstUpdate = true;
-
   private subscriptions = [];
 
   constructor(
@@ -139,8 +127,10 @@ export class CurrencyComponent implements OnInit, OnDestroy {
     this.syncService.forceCurrency(this.model.currencyInfo.id);
 
     // get transaction history
-    this.wallet.wallet.subscribe(async wallet => {
-      if (wallet) {
+    this.subscriptions.push(
+      this.wallet.wallet.pipe(
+        filter(wallet => !!wallet)
+      ).subscribe(async wallet => {
         this.isLoadingTransactions = true;
         try {
           const transactions = await wallet.getTransactions(this.step, 0);
@@ -148,22 +138,7 @@ export class CurrencyComponent implements OnInit, OnDestroy {
         } finally {
           this.isLoadingTransactions = false;
         }
-      }
-    });
-
-    /** @todo Refactor the stuff below */
-
-    // this.transactions = toBehaviourSubject(this.balanceWatcher.pipe(
-    //   mergeMap(async () => {
-    //     return await this.listTransactionHistory(this.step, 0);
-    //   })
-    // ), null);
-
-    // this.transactionsCount += this.step;
-
-    // interval(10000).subscribe(async () => {
-    //   await this.updateTransactions();
-    // });
+    }));
   }
 
   ngOnDestroy() {
@@ -202,71 +177,6 @@ export class CurrencyComponent implements OnInit, OnDestroy {
     }
   }
 
-  /** @todo Refactor the stuff below */
-
-  // public async listTransactionHistory(listTo, listFrom) {
-  //   const wallet = this.balanceWatcher.getValue() ? this.balanceWatcher.getValue().wallet : null;
-  //   if (wallet === null) {
-  //     return null;
-  //   }
-
-  //   if (this.transactionArray !== null) {
-  //     console.log('cached: ' + this.transactionArray.length);
-  //   }
-
-  //   if (this.transactionArray === null) {
-  //     const txs = await wallet.getTransactions(listTo, listFrom);
-  //     const txsMapped = txs.map(tx => HistoryEntry.fromJSON(tx));
-
-  //     this.transactionArray = txsMapped;
-  //   } else if (listFrom === 0) {
-  //     const txs = await wallet.getTransactions(listTo, listFrom);
-  //     const txsMapped = txs.map(tx => HistoryEntry.fromJSON(tx));
-
-  //     if (txsMapped.length > 0) {
-  //       if (txsMapped.slice(-1)[0].time <= this.transactionArray[0].time) {
-  //         const timeEnd = this.transactionArray[0].time;
-  //         const filtered = txsMapped.filter(item =>
-  //           (item.confirmed && item.time > timeEnd || !item.confirmed && this.transactionHashNotInList(item.blockhash))
-  //         );
-
-  //         this.transactionArray.unshift.apply(this.transactionArray, filtered);
-  //       } else {
-  //         this.timeEnd = this.transactionArray[0].time;
-  //         this.transactionArray.unshift.apply(this.transactionArray, txsMapped);
-  //       }
-  //     }
-  //   } else {
-  //     if (this.timeEnd !== null) {
-  //       const txs = await wallet.getTransactions(listTo, listFrom);
-  //       const txsMapped = txs.map(tx => HistoryEntry.fromJSON(tx));
-
-  //       if (txsMapped.slice(-1)[0].time <= this.transactionArray[0].time) {
-  //         const timeEnd = this.transactionArray[0].time;
-  //         const filtered = txsMapped.filter(item =>
-  //           (item.confirmed && item.time > timeEnd || !item.confirmed && this.transactionHashNotInList(item.blockhash))
-  //         );
-
-  //         Array.prototype.splice.apply(this.transactionArray, [listTo, 0].concat(filtered));
-  //         this.timeEnd = null;
-  //       } else {
-  //         Array.prototype.splice.apply(this.transactionArray, [listTo, 0].concat(txsMapped));
-  //       }
-  //     } else if (this.transactionArray.length < listTo && !this.isHistoryLoaded) {
-  //       const txs = await wallet.getTransactions(listTo, listFrom);
-  //       const txsMapped = txs.map(tx => HistoryEntry.fromJSON(tx));
-  //       const filtered = txsMapped.filter(item => (this.transactionHashNotInList(item.blockhash)));
-  //       this.transactionArray.push.apply(this.transactionArray, filtered);
-  //     }
-  //   }
-
-  //   if (this.transactionArray.length >= listTo) {
-  //     return this.transactionArray.slice(listFrom, listTo);
-  //   } else {
-  //     return this.transactionArray.slice(listFrom);
-  //   }
-  // }
-
   async loadMore() {
     if (this.isLoadingTransactions) {
       return;
@@ -283,106 +193,5 @@ export class CurrencyComponent implements OnInit, OnDestroy {
       this.isLoadingTransactions = false;
     }
 
-  //   if (!this.isHistoryLoaded && !this.isLoadingTransactions) {
-  //     this.isLoadingTransactions = true;
-
-  //     const newList = await this.listTransactionHistory(this.transactionsCount + this.step, this.transactionsCount);
-  //     const oldList = this.transactions.getValue();
-
-  //     if (newList.length > 0) {
-  //       oldList.push.apply(oldList, newList);
-
-  //       const unconfirmed = newList.filter(item => item.confirmed === false);
-  //       this.unconfirmedList.push.apply(this.unconfirmedList, unconfirmed);
-  //     }
-  //     if (newList.length < this.step) {
-  //       this.isHistoryLoaded = true;
-  //       console.log('History loaded!');
-  //     }
-
-  //     this.transactions.next(oldList);
-
-  //     this.transactionsCount += this.step;
-
-  //     this.isLoadingTransactions = false;
-  //   }
   }
-
-  // async updateTransactions(listTo = this.step, listFrom = 0) {
-  //   const wallet = this.balanceWatcher.getValue() ? this.balanceWatcher.getValue().wallet : null;
-  //   if (wallet !== null &&
-  //       this.transactions.getValue() !== null &&
-  //       !this.isUpdatingTransactions &&
-  //       !this.isLoadingTransactions
-  //   ) {
-  //     this.isUpdatingTransactions = true;
-
-  //     if (this.isFirstUpdate) {
-  //       const unconfirmed = this.transactions.getValue().filter(item => item.confirmed === false);
-  //       this.unconfirmedList.push.apply(this.unconfirmedList, unconfirmed);
-  //       this.isFirstUpdate = false;
-  //     }
-
-  //     const newList = await this.listTransactionHistory(listTo, listFrom);
-
-  //     if (newList !== null && newList.length > 0) {
-  //       this.checkUnconfirmed(newList);
-
-  //       const oldList = this.transactions.getValue();
-
-  //       if (newList.slice(-1)[0].time <= oldList[0].time) {
-  //         const timeEnd = oldList[0].time;
-  //         const filtered = newList.filter(item =>
-  //           (item.confirmed && item.time > timeEnd || !item.confirmed && this.transactionHashNotInList(item.blockhash))
-  //         );
-
-  //         oldList.unshift.apply(oldList, filtered);
-  //         this.transactions.next(oldList);
-
-  //         const unconfirmed = filtered.filter(item => item.confirmed === false);
-  //         this.unconfirmedList.unshift.apply(this.unconfirmedList, unconfirmed);
-  //       } else {
-  //         oldList.unshift.apply(oldList, newList);
-  //         this.transactions.next(oldList);
-
-  //         const unconfirmed = newList.filter(item => item.confirmed === false);
-  //         this.unconfirmedList.unshift.apply(this.unconfirmedList, unconfirmed);
-
-  //         this.updateTransactions(listTo + this.step, listFrom + this.step);
-  //       }
-  //     }
-
-  //     this.isUpdatingTransactions = false;
-  //   }
-  // }
-
-  // public transactionHashNotInList(blockhash: string): boolean {
-  //   const transactionsList = this.transactions.getValue();
-  //   for (let i = 0; i < transactionsList.length; i++) {
-  //     if (transactionsList[i].blockhash === blockhash) {
-  //       return false;
-  //     }
-  //   }
-
-  //   return true;
-  // }
-
-  // public checkUnconfirmed(newList: Array<HistoryEntry>): void {
-  //   if (this.unconfirmedList.length > 0) {
-  //     const oldList = this.transactions.getValue();
-  //     const confirmedList = newList.filter(item => item.confirmed === true);
-  //     for (const confirmed of confirmedList) {
-  //       const unconfirmed = this.unconfirmedList.filter(item => item.blockhash === confirmed.blockhash);
-  //       if (unconfirmed.length > 0) {
-  //         const unconfirmedEntity = unconfirmed[0];
-  //         this.unconfirmedList.splice(this.unconfirmedList.indexOf(unconfirmedEntity), 1);
-
-  //         const confirmedEntity = oldList.filter(item => item.blockhash === confirmed.blockhash)[0];
-  //         confirmedEntity.confirmed = true;
-  //         confirmedEntity.time = confirmed.time;
-  //       }
-  //     }
-  //     this.transactions.next(oldList);
-  //   }
-  // }
 }
