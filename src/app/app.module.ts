b1227570ed35ba2e41530e8d8c81bbfc687682bf
Roman <roman@caspiantechnologies.org>
2018-10-15 10:01:34 +0700

    Open/close navbar by swipe gesture (fixed wallet scrolling)
    SPATIUM-230

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import {
   MatSnackBarModule,
   MatToolbarModule,
 } from '@angular/material';
-import { BrowserModule } from '@angular/platform-browser';
+import { HAMMER_GESTURE_CONFIG, BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { QRCodeModule } from 'angular2-qrcode';
 import { AppRoutingModule } from './app-routing.module';
@@ -119,6 +119,7 @@ import { RPCConnectionService } from './services/rpc/rpc-connection.service';
 import { DeviceDiscoveryComponent } from './screens/navigator/device-discovery/device-discovery.component';
 import { ConnectedDeviceComponent } from './elements/connectivity-manage/connected-device/connected-device.component';
 import { NetworkService } from './services/network.service';
+import { HammerConfig } from './configs/hammer-config';
 
 @NgModule({
   declarations: [
@@ -248,7 +249,8 @@ import { NetworkService } from './services/network.service';
     BalanceService,
     PriceService,
     RPCConnectionService,
-    NetworkService
+    NetworkService,
+    {provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig}
   ],
   bootstrap: [
     AppComponent
