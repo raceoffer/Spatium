73b302e9d069e613820eef12098fdce3aaf357bc
Roman <roman@caspiantechnologies.org>
2018-10-29 14:06:12 +0700

    Display Wi-Fi status changes in Wi-Fi component
    SPATIUM-391

--- src/app/elements/connectivity-manage/wifi/wifi.component.ts
+++ src/app/elements/connectivity-manage/wifi/wifi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import { DeviceService, Platform } from '../../../services/device.service';
 import { WiFiService } from '../../../services/wifi.service';
 
@@ -10,10 +10,18 @@ declare const cordova: any;
   styleUrls: ['./wifi.component.css', '../connectivity-manage.css']
 })
 export class WiFiComponent implements OnInit, OnDestroy {
+  public available: boolean;
+  public enabled: boolean;
+  public connected: boolean;
 
   constructor(
+    private readonly ngZone: NgZone,
     private readonly deviceService: DeviceService,
-    public readonly wifiService: WiFiService) { }
+    private readonly wifiService: WiFiService) {
+    wifiService.available.subscribe(available => this.ngZone.run(() => this.available = available));
+    wifiService.enabled.subscribe(enabled => this.ngZone.run(() => this.enabled = enabled));
+    wifiService.connected.subscribe(connected => this.ngZone.run(() => this.connected = connected));
+  }
 
   async ngOnInit() { }
 
--- src/app/services/wifi.service.ts
+++ src/app/services/wifi.service.ts
@@ -1,21 +1,46 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {DeviceService} from './device.service';
 
 declare const cordova: any;
 
 @Injectable()
 export class WiFiService {
-    public available: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public enabled: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    public connected: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public available: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public enabled: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public connected: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-    constructor() {
-        cordova.plugins.ssdp.isAvailable().then(available => this.available.next(available));
-        cordova.plugins.ssdp.isEnabled().then(enabled => this.enabled.next(enabled));
-        cordova.plugins.ssdp.isConnected().then(connected => this.connected.next(connected));
+  constructor(private readonly deviceService: DeviceService) {
+    this.init();
+  }
 
-        cordova.plugins.ssdp.setAvailabilityChangedCallback(available => this.available.next(available));
-        cordova.plugins.ssdp.setAdapterStatusChangedCallback(enabled => this.enabled.next(enabled));
-        cordova.plugins.ssdp.setConnectionChangedCallback(connected => this.connected.next(connected));
+  private async init() {
+    await this.deviceService.deviceReady();
+
+    cordova.plugins.ssdp.isAvailable().then(available => this.updateAvailabilityStatus(available));
+    cordova.plugins.ssdp.isEnabled().then(enabled => this.updateAdapterStatus(enabled));
+    cordova.plugins.ssdp.isConnected().then(connected => this.updateConnectionStatus(connected));
+
+    cordova.plugins.ssdp.setAvailabilityChangedCallback(available => this.updateAvailabilityStatus(available));
+    cordova.plugins.ssdp.setAdapterStatusChangedCallback(enabled => this.updateAdapterStatus(enabled));
+    cordova.plugins.ssdp.setConnectionChangedCallback(connected => this.updateConnectionStatus(connected));
+  }
+
+  private updateAvailabilityStatus(available: boolean) {
+    if (available !== this.available.getValue()) {
+      this.available.next(available);
     }
+  }
+
+  private updateAdapterStatus(enabled: boolean) {
+    if (enabled !== this.enabled.getValue()) {
+      this.enabled.next(enabled);
+    }
+  }
+
+  private updateConnectionStatus(connected: boolean) {
+    if (connected !== this.connected.getValue()) {
+      this.connected.next(connected);
+    }
+  }
 }
